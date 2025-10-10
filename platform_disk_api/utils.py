d36a4903062729c6ac3117b483d92e20cc262273
Petrucci <petrucci@ua.fm>
2025-10-10 17:12:46 +0300

    fixes

--- platform_disk_api/utils.py
+++ platform_disk_api/utils.py
@@ -1,6 +1,22 @@
 from datetime import UTC, datetime, timedelta
 
 
+SUFFIX_TO_FACTOR = {
+    "E": 10**18,
+    "P": 10**15,
+    "T": 10**12,
+    "G": 10**9,
+    "M": 10**6,
+    "k": 10**3,
+    "Ei": 1024**6,
+    "Pi": 1024**5,
+    "Ti": 1024**4,
+    "Gi": 1024**3,
+    "Mi": 1024**2,
+    "Ki": 1024,
+}
+
+
 def utc_now() -> datetime:
     return datetime.now(UTC)
 
@@ -24,24 +40,10 @@ def timedelta_load(raw: str) -> timedelta:
 def _storage_str_to_int(storage: str) -> int:
     # More about this format:
     # https://github.com/kubernetes/kubernetes/blob/6b963ed9c841619d511d2830719b6100d6ab1431/staging/src/k8s.io/apimachinery/pkg/api/resource/quantity.go#L30
-    suffix_to_factor = {
-        "E": 10**18,
-        "P": 10**15,
-        "T": 10**12,
-        "G": 10**9,
-        "M": 10**6,
-        "k": 10**3,
-        "Ei": 1024**6,
-        "Pi": 1024**5,
-        "Ti": 1024**4,
-        "Gi": 1024**3,
-        "Mi": 1024**2,
-        "Ki": 1024,
-    }
     try:
         return int(float(storage))
     except ValueError:
-        for suffix, factor in suffix_to_factor.items():
+        for suffix, factor in SUFFIX_TO_FACTOR.items():
             if storage.endswith(suffix):
                 return factor * int(storage[: -len(suffix)])
         raise
